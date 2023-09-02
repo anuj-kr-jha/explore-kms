06c6bc900cc42a5cd638c251d454d8cfeec97b0e
Anuj Kumar Jha <anuj.kr.jha@protonmail.com>
2023-09-02 13:26:07 +0530

    updated  notes

--- index.mjs
+++ index.mjs
@@ -19,16 +19,28 @@ async function main() {
 main();
 
 /*
- user registration and login flow
-  1. user registration
-    - user enters email and password
-    - password is encrypted via kms and then stored in db
-  2. user login
-    - user enters email and password
-    - encrypted password fetched from db
-    - encrypted password is decrypted via kms
-    - decrypted password is compared with user input
-    - if match, user should be logged in
-    - jwt_token is generated and sent to user
-    - user can use jwt_token to access protected routes
+  ! user registration and login flow
+      1. user registration
+        - user enters email and password
+        - password is encrypted via kms and then stored in db
+      2. user login
+        - user enters email and password
+        - encrypted password fetched from db
+        - encrypted password is decrypted via kms
+        - decrypted password is compared with user input
+        - if match, user should be logged in
+        - jwt_token is generated and sent to user
+        - user can use jwt_token to access protected routes
+*/
+
+/*
+  !WARNING:
+    - kms is used to encrypt and decrypt password
+    - so that password is not stored in plain text in db
+    - but we can decrypt password using kms
+
+    - It is recommended to use hashing algorithm for password instead of encryption/decryption
+    - and then store the hashed password in db
+    - so that password cannot be decrypted as hashing is one way
+
 */
