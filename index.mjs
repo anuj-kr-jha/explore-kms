c661320451ee2eae86b671f424445be1ba102c9f
Anuj Kumar Jha <anuj.kr.jha@protonmail.com>
2023-09-02 13:20:05 +0530

    added flow notes

--- index.mjs
+++ index.mjs
@@ -17,3 +17,18 @@ async function main() {
 }
 
 main();
+
+/*
+ user registration and login flow
+  1. user registration
+    - user enters email and password
+    - password is encrypted via kms and then stored in db
+  2. user login
+    - user enters email and password
+    - encrypted password fetched from db
+    - encrypted password is decrypted via kms
+    - decrypted password is compared with user input
+    - if match, user should be logged in
+    - jwt_token is generated and sent to user
+    - user can use jwt_token to access protected routes
+*/
